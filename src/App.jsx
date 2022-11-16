e6d73f944861dc85a9cce2b6270d1a784f2e74f6
David Brockbank <broda677@student.otago.ac.nz>
2022-11-16 13:41:21 +1300

    Navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 // import './App.scss'
-import Sidebar from './styles/components/Sidebar.jsx'
+import Navbar from './styles/components/Sidebar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Sidebar></Sidebar>
+      <Navbar></Navbar>
     </>
   )
 }
--- src/styles/components/Sidebar.jsx
+++ src/styles/components/Sidebar.jsx
@@ -1,9 +1,19 @@
 
 
 
-function Sidebar() {
+function Navbar() {
 
-    return (<h1 className="">HI</h1>);
+    return (
+        <nav className="bg-slate-600 p-3 items-center">
+            <div className="h-full container items-center text-2xl flex align- flex-wrap justify-between mx-auto">
+                <h1 className="font-bold">David Brockbank</h1>
+                <button>Projects</button>
+                <button>Education</button>
+                <button>About</button>
+            </div>
+
+        </nav>
+    );
 }
 
-export default Sidebar
+export default Navbar
