4c6ccc9c57a1ccc14c227a43db57749b6d73d272
David Brockbank <broda677@student.otago.ac.nz>
2022-11-16 13:13:48 +1300

    intit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import './App.scss'
+// import './App.scss'
+import Sidebar from './styles/components/Sidebar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Sidebar></Sidebar>
     </>
   )
 }
---
+++ src/styles/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+
+
+
+function Sidebar() {
+
+    return (<h1 className="">HI</h1>);
+}
+
+export default Sidebar
